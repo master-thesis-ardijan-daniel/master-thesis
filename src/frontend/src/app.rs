9433825606e950f312a4ef58542355f389453828
danielalvsaaker <30574112+danielalvsaaker@users.noreply.github.com>
2025-03-17 12:55:43 +0100

    clippy

--- src/frontend/src/app.rs
+++ src/frontend/src/app.rs
@@ -3,7 +3,6 @@ use std::sync::Arc;
 use web_time::Duration;
 use winit::{
     application::ApplicationHandler,
-    dpi::PhysicalSize,
     event::{StartCause, WindowEvent},
     event_loop::{ActiveEventLoop, EventLoopProxy},
     window::WindowAttributes,
