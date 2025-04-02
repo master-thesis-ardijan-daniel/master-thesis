90ec416e8120732a4894ffb37d21104fc8b21927
danielalvsaaker <30574112+danielalvsaaker@users.noreply.github.com>
2025-04-02 14:26:59 +0200

    wip 2

--- src/backend/src/main.rs
+++ src/backend/src/main.rs
@@ -1,5 +1,8 @@
 use axum::Router;
-use std::net::SocketAddr;
+use std::{
+    net::SocketAddr,
+    ops::{Index, Range},
+};
 use tower_http::{services::ServeDir, trace::TraceLayer};
 
 #[tokio::main]
@@ -26,12 +29,12 @@ struct Coordinates {
 }
 
 struct LeafNode<T> {
-    bounds: Coordinates,
+    bounds: Bounds,
     value: T,
 }
 
 struct ParentNode<T> {
-    bounds: Coordinates,
+    bounds: Bounds,
     children: Vec<Node<T>>,
 }
 
@@ -39,14 +42,72 @@ struct GeoTree<T> {
     root: Node<T>,
 }
 
-impl<T> GeoTree<T> {
-    fn create<F>(input_data: Vec<T>, aggregation_function: F) -> Self
-    where
-        F: FnMut(Vec<T>) -> T,
-    {
+struct Bounds {
+    north_west: Coordinates,
+    south_east: Coordinates,
+}
 
-        let parent = aggregation_function()
+impl<T> GeoTree<T>
+where
+    T: Dataset<T>,
+{
+    fn create(input_data: Vec<Vec<T>>, bounds: Bounds) -> Self {
+        let root = ParentNode {
+            bounds,
+            children: Vec::<Node<T>>::new(),
+        };
+
+        // assert_eq!(T::MASK_DIM. % 4, 0);
+        //
+        //
+
+        let mut s = vec![];
+
+        for y in 0..input_data.len() / T::MASK_DIM.1 {
+            let y = y * T::MASK_DIM.1;
+            let y_mask = input_data.safe_slice(y..y + T::MASK_DIM.1);
+            for x in 0..input_data[y].len() / T::MASK_DIM.0 {}
+        }
 
         todo!()
     }
+}
+
+// impl Dataset<f32> for f32 {
+//     fn aggregate(data: Vec<Self>) -> Self {
+//         data.iter().sum()
+//     }
+
+//     const MASK_DIM: (usize, usize) = todo!();
+// }
+
+trait SliceExt<T> {
+    fn safe_slice(&self, range: Range<usize>) -> &[T];
+}
+
+impl<T> SliceExt<T> for Vec<T> {
+    fn safe_slice(&self, range: Range<usize>) -> &[T] {
+        let start = range.start;
+        let mut end = range.end;
+
+        loop {
+            if let Some(slice) = self.get(start..end) {
+                return slice;
+            }
+
+            end -= 1;
+
+            if start == end {
+                return &[];
+            }
+        }
+    }
+}
+
+trait Dataset<T> {
+    fn aggregate(data: Vec<T>) -> T;
+
+    /// Value must be a power of 4.
+    const MASK_DIM: (usize, usize);
+    const STRIDE: usize;
 }
