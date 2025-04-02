e756ee321a500694e6ace4e92510c66a938e9ef4
ardijanr <ardijan@timeanddate.com>
2025-04-02 13:13:08 +0200

    wip

--- src/backend/src/main.rs
+++ src/backend/src/main.rs
@@ -14,3 +14,39 @@ async fn main() {
         .await
         .unwrap();
 }
+
+enum Node<T> {
+    Leaf(LeafNode<T>),
+    Parent(ParentNode<T>),
+}
+
+struct Coordinates {
+    lat: f32,
+    lon: f32,
+}
+
+struct LeafNode<T> {
+    bounds: Coordinates,
+    value: T,
+}
+
+struct ParentNode<T> {
+    bounds: Coordinates,
+    children: Vec<Node<T>>,
+}
+
+struct GeoTree<T> {
+    root: Node<T>,
+}
+
+impl<T> GeoTree<T> {
+    fn create<F>(input_data: Vec<T>, aggregation_function: F) -> Self
+    where
+        F: FnMut(Vec<T>) -> T,
+    {
+
+        let parent = aggregation_function()
+
+        todo!()
+    }
+}
